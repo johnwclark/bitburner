faef3fe9c7b849dd78bfb18d7e4adc384d44c62f
John W. Clark <john.wilbur.clark@gmail.com>
2023-06-26 18:42:52 -0500

    Create test.js
    start of hacking test scripts

---
+++ test.js
@@ -0,0 +1,11 @@
+/** @param {NS} ns */
+export async function main(ns) {
+  if ( ns.args.length < 1 ) {
+    ns.tprint("no args given")
+    ns.exit(1);
+  }
+  let target = ns.args[0];
+  ns.exec("hack-target.js","home",1,target );
+  ns.exec("deploy-local.js","home",1,target );
+
+}
