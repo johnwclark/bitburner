1b1814bd2dca46fd28d348480e195328babadd5f
John W. Clark <john.wilbur.clark@gmail.com>
2023-06-26 18:41:25 -0500

    Create start-hacking.js
    a script to nuke and deploy local to all hackable servers

---
+++ start-hacking.js
@@ -0,0 +1,56 @@
+/** @param {NS} ns */
+export async function main(ns) {
+  let scanRval = recursiveScan(ns);
+  ns.tprint( scanRval );
+
+  for( let i = 0 ; i < scanRval.length ; ++i ) {
+    if ( ! scanRval[i].startsWith("pserv") && scanRval[i] != "home" ) {
+      let target = scanRval[i];
+      if ( ns.getHackingLevel() > ns.getServerSecurityLevel(target) ) {
+        ns.tprint("working on host : " + target);
+        ns.exec("hack-target.js","home",1,target );
+        ns.exec("deploy-local.js","home",1,target );
+      } else {
+        ns.tprint("host hacking level too high : " + target + "(" + ns.getServerSecurityLevel(target) + ")");
+      }
+    }
+  }
+
+}
+
+
+/** @param {NS} ns */
+export function recursiveScan(ns){
+	let i = 0;
+	let servers = ns.scan("home");
+	let depth = determineRecurse(ns);
+	
+	let serverSet = new Set(servers);
+
+	while (i < depth){
+
+		for (let serverName of Array.from(serverSet)) {
+			
+			serverSet.add(serverName)
+			let networkScan = ns.scan(serverName);
+
+			for (let hostName of networkScan){
+				serverSet.add(hostName)
+			}
+		}
+		i++;
+	}
+	return (Array.from(serverSet))
+}
+
+/** @param {NS} ns */
+export function determineRecurse(ns){
+	let recursive = 2;
+	if (ns.fileExists("DeepscanV1.exe")) {
+		recursive = 5;
+	}
+	if (ns.fileExists("DeepscanV2.exe")) {
+		recursive = 10;
+	}
+	return recursive;
+}
