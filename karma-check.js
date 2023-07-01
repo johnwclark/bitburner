19482d4dfe8c0cfca65c7178d54ec69c85035fc7
John W. Clark <john.wilbur.clark@gmail.com>
2023-07-01 10:18:36 -0500

    fixed error in killed counts

--- karma-check.js
+++ karma-check.js
@@ -4,7 +4,7 @@ export async function main(ns) {
   let karma = ns.heart.break();
   ns.tprint("karma = " + karma);
 
-  let killed = ns.getPlayer().killed;
+  let killed = ns.getPlayer().numPeopleKilled;
   ns.tprint("killed = " + killed);
 
 }
