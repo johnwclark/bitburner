00e8d93634b1bb0d40af7954547b145dd0f02abb
John W. Clark <john.wilbur.clark@gmail.com>
2023-07-04 10:44:05 -0500

    updates to script to self-hack

--- local.js
+++ local.js
@@ -1,15 +1,15 @@
 /** @param {NS} ns */
 export async function main(ns) {
-  let target = ns.getHostname();
+  const target = ns.getHostname();
   //ns.tprint("target = " + target);
 
   // only hack if it has 75% of the server's max money
-  let moneyThresh = ns.getServerMaxMoney(target) * 0.75;
+  const moneyThresh = ns.getServerMaxMoney(target) * 0.95;
   
   // Defines the maximum security level the target server can
   // have. If the target's security level is higher than this,
   // we'll weaken it before doing anything else
-  let securityThresh = ns.getServerMinSecurityLevel(target) + 5;
+  const securityThresh = ns.getServerMinSecurityLevel(target) + 2;
   
   // Infinite loop that continously hacks/grows/weakens the target server
   while(true) {
